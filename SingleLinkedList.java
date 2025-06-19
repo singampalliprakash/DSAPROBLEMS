e70178f24e74f384953aede3988adc14e043692c
Jnana prakash Singampalli <jnanaprakash2002@gmail.com>
2025-06-19 22:20:53 +0530

    Add files via upload

---
+++ SingleLinkedList.java
@@ -0,0 +1,80 @@
+package Demo;
+
+public class SingleLinkedList
+{
+	Node head;
+	class Node
+	{
+		
+	String data;
+	Node nextNode;
+	public Node(String data)
+	{
+		this.data=data;
+		this.nextNode=null;
+	}
+	public String toString()
+	{
+		return data;
+	}
+	}
+	
+	public void insertFirst(String data) 
+	{
+		Node newnode=new Node(data);
+		if(head==null)
+		{
+			head=newnode;
+			return;
+		}
+		else
+		{
+			newnode.nextNode=head;
+			head=newnode;
+		}
+		
+		
+	}
+	public void insertLast(String data) 
+	{	
+		Node newnode=new Node(data);
+		if(head==null)
+		{
+			head=newnode;
+			return;
+		}
+		Node currentnode=head;
+		while (currentnode.nextNode!=null) {
+			currentnode=currentnode.nextNode;
+			
+		}
+		currentnode.nextNode=newnode;
+	}
+	public void printList()
+	{
+		if(head==null)
+		{
+			System.out.println("there is no elements present");
+		}
+		Node currentNode=head;
+		while(currentNode!=null)
+		{
+			System.out.println(currentNode);
+			currentNode=currentNode.nextNode;
+		}
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		SingleLinkedList list=new SingleLinkedList();
+		list.insertFirst("java");
+		list.insertFirst("python");
+		list.insertLast("spring");
+		list.insertFirst("dotnet");
+		list.printList();
+		System.out.println(list.head);
+
+	}
+
+}
